102bc7c9cf9f2da7b648e62900b97e7dc690dfcb
SANTANGELO LUCA <lucasantangelo723@gmail.com>
2026-05-29 13:00:02 +0200

    Fix NotificationObserver
    Implementazione metodo NotificationObserver

--- Backend/api/src/main/java/com/bugboard/api/observer/NotificationObserver.java
+++ Backend/api/src/main/java/com/bugboard/api/observer/NotificationObserver.java
@@ -2,16 +2,18 @@ package com.bugboard.api.observer;
 
 import com.bugboard.api.models.Issue;
 import com.bugboard.api.models.User;
+import com.bugboard.api.services.NotificationService;
 
 public class NotificationObserver implements Observer {
 
-//    private final NotificationService notificationService;
-//    public NotificationObserver(NotificationService notificationService) {
-//        this.notificationService = notificationService;
-//    }
+    private final NotificationService notificationService;
+    public NotificationObserver(NotificationService notificationService) {
+        this.notificationService = notificationService;
+    }
 
     @Override
     public void update(Issue issue, User user) {
-        // usa metodo notificationService
+        
+        notificationService.createNotification(issue, user);
     }
 }
