36464db2ccebca664d076db396381da9c1a237a3
SANTANGELO LUCA <lucasantangelo723@gmail.com>
2026-05-29 12:09:37 +0200

    Create IssueObserverComponent.java

---
+++ Backend/api/src/main/java/com/bugboard/api/services/IssueObserverComponent.java
@@ -0,0 +1,32 @@
+package com.bugboard.api.services;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.bugboard.api.models.Issue;
+import com.bugboard.api.models.User;
+import com.bugboard.api.observer.Observer;
+
+@Component
+public class IssueObserverComponent {
+
+    private final List<Observer> observers = new ArrayList<>();
+
+    public void attach(Observer observer) {
+        observers.add(observer);
+    }
+
+        public void detach(Observer observer) {
+        observers.remove(observer);
+    }
+
+    public void notifyObservers(Issue issue, User user) {
+        for (Observer observer : observers) {
+            observer.update(issue, user);
+        }
+    }
+
+}
+
