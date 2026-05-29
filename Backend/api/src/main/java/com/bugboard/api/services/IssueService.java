58637e27e3c99ab27c42ef9f09022b22b4a658e0
SANTANGELO LUCA <lucasantangelo723@gmail.com>
2026-05-29 12:26:31 +0200

    Delete metodi e campo  Observer da IssueService

--- Backend/api/src/main/java/com/bugboard/api/services/IssueService.java
+++ Backend/api/src/main/java/com/bugboard/api/services/IssueService.java
@@ -3,14 +3,12 @@ package com.bugboard.api.services;
 import com.bugboard.api.dto.CreateIssueDTO;
 import com.bugboard.api.dto.IssueDTO;
 import com.bugboard.api.models.*;
-import com.bugboard.api.observer.Observer;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 public interface IssueService {
 
-    List<Observer> observers=new ArrayList<>();
+    
 
     IssueDTO createIssue(CreateIssueDTO dto);
 
@@ -21,12 +19,6 @@ public interface IssueService {
     List<IssueDTO> searchIssueByTitleOrDescription(String keyword);
 
     List<IssueDTO> findByAssignedToUuid(UUID assignedTo);
-
-    void attach(Observer observer);
-
-    void detach(Observer observer);
-
-    void notifyObservers(Issue issue, User user);
 
     void assignIssue(UUID issueUuid, UUID userUuid);
 }
