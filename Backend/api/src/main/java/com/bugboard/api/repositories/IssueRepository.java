bd93a3601fde64466ebe957b1ab5ac3ccce35c95
ManuelMontuori <mmontuori@live.it>
2026-06-10 12:28:09 +0200

    Cancellazione metodi inutili
    Dimenticanza sul commit precedente

--- Backend/api/src/main/java/com/bugboard/api/repositories/IssueRepository.java
+++ Backend/api/src/main/java/com/bugboard/api/repositories/IssueRepository.java
@@ -13,25 +13,6 @@ import java.util.UUID;
 
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long>, JpaSpecificationExecutor<Issue> {
-    // soliti metodi CRUD sono già disponibili grazie a JpaRepository
-
-    List<Issue> findByStatus(IssueStatus status);
-
-    List<Issue> findByPriority(IssuePriority priority);
-
-    List<Issue> findByType(IssueType type);
-
-    List<Issue> findByStatusAndPriority(IssueStatus status, IssuePriority priority);
-
-    List<Issue> findByStatusAndType(IssueStatus status, IssueType type);
-
-    List<Issue> findByPriorityAndType(IssuePriority priority, IssueType type);
-
-    List<Issue> findByStatusAndPriorityAndType(
-            IssueStatus status,
-            IssuePriority priority,
-            IssueType type
-    );
 
     List<Issue> findByTitleContainingIgnoreCaseOrDescriptionContainingIgnoreCase(
             String titleKeyword,
