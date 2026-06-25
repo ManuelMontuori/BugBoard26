99c42ed653c0124b874eddd97275cdde4efc3c2e
SANTANGELO LUCA <lucasantangelo723@gmail.com>
2026-06-25 09:26:09 +0200

    Primi passi Testing
    Ho aggiunto le dipendenze per il testing nel pom e scritto un primo test sul metodo assignIssue creando il package service in Test

---
+++ Backend/api/src/test/java/com/bugboard/api/services/IssueWriteServiceTest.java
@@ -0,0 +1,86 @@
+package com.bugboard.api.services;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.context.ApplicationEventPublisher;
+
+import com.bugboard.api.auth.AuthService;
+import com.bugboard.api.events.IssueAssignedEvent;
+import com.bugboard.api.mapper.IssueMapper;
+import com.bugboard.api.models.Issue;
+import com.bugboard.api.models.User;
+import com.bugboard.api.repositories.IssueRepository;
+import com.bugboard.api.repositories.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class IssueWriteServiceTest {
+
+    @Mock
+    IssueRepository issueRepository;
+
+    @Mock
+    UserRepository userRepository;
+
+    @Mock
+    AuthService authService;
+
+    @Mock
+    IssueMapper issueMapper;
+
+    @Mock
+    ApplicationEventPublisher eventPublisher;
+
+    @InjectMocks
+    IssueWriteService issueWriteService;
+
+    //caso positivo (utente esiste)
+    @Test
+    void testAssignIssue_validUser() {
+        UUID issueId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        Issue issue = new Issue();
+        User user = new User();
+
+        when(issueRepository.findByUuid(issueId)).thenReturn(issue);
+        when(userRepository.findByUuid(userId)).thenReturn(Optional.of(user));
+
+        issueWriteService.assignIssue(issueId, userId);
+
+        // Verifiche (ORACLE)
+        assertEquals(user, issue.getAssignedTo());
+        assertNotNull(issue.getAssignedAt());
+
+        verify(eventPublisher).publishEvent(any(IssueAssignedEvent.class));
+    }
+
+    // utente inesistente
+    @Test
+    void testAssignIssue_userNotFound() {
+        UUID issueId = UUID.randomUUID();
+        UUID userId = UUID.randomUUID();
+
+        Issue issue = new Issue();
+
+        when(issueRepository.findByUuid(issueId)).thenReturn(issue);
+        when(userRepository.findByUuid(userId)).thenReturn(Optional.empty());
+
+        assertThrows(IllegalArgumentException.class, () -> {
+            issueWriteService.assignIssue(issueId, userId);
+        });
+    }
+
+}
