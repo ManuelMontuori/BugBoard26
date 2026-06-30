18fc9dfefec19718b3b58b87a863dffafb22c56d
SANTANGELO LUCA <lucasantangelo723@gmail.com>
2026-06-30 14:27:24 +0200

    Cancellazione @repository

--- Backend/api/src/main/java/com/bugboard/api/repositories/NotificationRepository.java
+++ Backend/api/src/main/java/com/bugboard/api/repositories/NotificationRepository.java
@@ -1,15 +1,13 @@
 package com.bugboard.api.repositories;
 
 import com.bugboard.api.models.Notification;
-
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
-
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
+
+
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
     Optional<Notification> findByUuid(UUID uuid);
     List<Notification> findByUserId(Long userId);
